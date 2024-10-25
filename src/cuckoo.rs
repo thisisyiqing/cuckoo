fdf2a1c9fa928790557e28c9a50b1f35880fc19c
thisisyiqing <marina_wyq@163.com>
2024-10-25 13:47:54 -0700

    concurrency should work now

--- src/cuckoo.rs
+++ src/cuckoo.rs
@@ -23,26 +23,26 @@ const INIT_SIZE: usize = 12;
 const MAX_RELOCS: usize = 12;
 
 pub struct CuckooHashTable<K, V> {
-    size: usize,
+    size: Arc<Mutex<usize>>,
     arr: Arc<Vec<Mutex<KeyVal<K, V>>>>,
 }
 
 impl<K: Serialize + Eq + Clone + std::fmt::Debug, V: Clone + std::fmt::Debug> CuckooHashTable<K, V> {
     pub fn new() -> Self {
         CuckooHashTable {
-            size: INIT_SIZE,
+            size: Arc::new(Mutex::new(INIT_SIZE)),
             arr: Arc::new((0..INIT_SIZE).map(|_| Mutex::new(KeyVal::new())).collect())
         }
     }
 
     fn hash1(&self, key: &K) -> usize {
         let key_bytes = bincode::serialize(key).unwrap();
-        rapidhash(&key_bytes.as_slice()) as usize % self.size
+        rapidhash(&key_bytes.as_slice()) as usize % *self.size.lock().unwrap()
     }
 
     fn hash2(&self, key: &K) -> usize {
         let key_bytes = bincode::serialize(key).unwrap();
-        gxhash64(&key_bytes.as_slice(), 14893) as usize % self.size
+        gxhash64(&key_bytes.as_slice(), 14893) as usize % *self.size.lock().unwrap()
     }
 
     fn insert_find_path(&self, key: K) -> Option<Vec<usize>> {
@@ -54,7 +54,6 @@ impl<K: Serialize + Eq + Clone + std::fmt::Debug, V: Clone + std::fmt::Debug> Cu
             let mut ele = queue.remove(0);
             let last_idx = ele.last().unwrap();
             let last_idx_key;
-
             // Lock the mutex for the last index
             {
                 let entry = self.arr[*last_idx].lock().unwrap();
@@ -78,25 +77,46 @@ impl<K: Serialize + Eq + Clone + std::fmt::Debug, V: Clone + std::fmt::Debug> Cu
         None
     }
 
-    pub fn insert(&mut self, key: K, value: V) -> Result<(), String> {
+    pub fn insert(&self, key: K, value: V) -> Result<(), String> {
         loop {
-            let path = self.insert_find_path(key.clone());
-            //TODO: validate the path
+            let path: Option<Vec<usize>> = self.insert_find_path(key.clone());
             if path.is_none() {
-                println!("Resizing required");
                 self.resize();
             } else {
                 let q: Vec<usize> = path.unwrap();
-                println!("{:?}", q);
+                println!("{:?} {:?}", key, q);
+                let mut invalid = false;
                 for i in (0..q.len() - 1).rev() {
-                    let current_entry = self.arr[q[i]].lock().unwrap();
+                    let mut current_entry = self.arr[q[i]].lock().unwrap();
                     let mut next_entry = self.arr[q[i + 1]].lock().unwrap();
+                    if next_entry.key.is_some() {
+                        invalid = true;
+                        println!("path is invalid so retrying...");
+                        break;
+                        // return Err("path is invalid".to_string());
+                    }
+                    let i1 = self.hash1(current_entry.key.as_ref().unwrap());
+                    let i2 = self.hash2(current_entry.key.as_ref().unwrap());
+                    if !(q[i + 1] == i1 || q[i + 1] == i2) {
+                        invalid = true;
+                        println!("path is invalid so retrying...");
+                        break;
+                        // return Err("path is invalid".to_string());
+                    }
                     next_entry.key = current_entry.key.clone();
                     next_entry.value = current_entry.value.clone();
+                    current_entry.key = None;
+                }
+                if invalid {
+                    continue;
                 }
 
                 {
                     let mut first_entry = self.arr[q[0]].lock().unwrap();
+                    if first_entry.key.is_some() {
+                        println!("path is invalid so retrying...");
+                        continue;
+                    }
                     first_entry.key = Some(key);
                     first_entry.value = Some(value);
                 }
@@ -106,23 +126,18 @@ impl<K: Serialize + Eq + Clone + std::fmt::Debug, V: Clone + std::fmt::Debug> Cu
         }
     }
 
-    // fn resize(&mut self) {
-    //     let old_table = self.arr.clone();
-    //     self.size *= 2;
-    //     self.arr = vec![KeyVal::new(); self.size];
-    
-    //     for entry in old_table.into_iter() {
-    //         if entry.key.is_some() {
-    //             self.insert(entry.key.unwrap(), entry.value.unwrap()).unwrap();
-    //         }
-    //     }
-    // }
-
-    fn resize(&mut self) {
+    fn resize(&self) -> Result<(), String> {
+        match self.size.try_lock() {
+            Ok(_) => {}
+            Err(_) => {
+                return Err("another resize in progress, aborting...".to_string());
+            }
+        }
+        let mut s = self.size.lock().unwrap();
         let old_table: Vec<KeyVal<K, V>>;
         {
             // Collect old entries while holding their locks
-            old_table = (0..self.size)
+            old_table = (0..*s)
                 .map(|i| {
                     let entry: std::sync::MutexGuard<'_, KeyVal<K, V>> = self.arr[i].lock().unwrap();
                     entry.clone()
@@ -131,23 +146,27 @@ impl<K: Serialize + Eq + Clone + std::fmt::Debug, V: Clone + std::fmt::Debug> Cu
         }
 
         // Resize the array
-        let new_size = self.size * 2;
+        let new_size = *s * 2;
         let new_arr: Vec<Mutex<KeyVal<K, V>>> = (0..new_size).map(|_| Mutex::new(KeyVal::new())).collect();
 
         // Update self.arr to the new array
         // Lock for exclusive access during resizing
-        let arr = Arc::get_mut(&mut self.arr).unwrap();
-        *arr = new_arr;
-        self.size = new_size;
-
+        unsafe {
+            let cloned = &mut Arc::clone(&self.arr);
+            let arr = Arc::get_mut_unchecked(cloned);
+            *arr = new_arr;
+        }
+        *s = new_size;
+        drop(s);
         // Reinsert the old table's entries
         for entry in old_table.into_iter() {
             if entry.key.is_some() {
                 self.insert(entry.key.unwrap(), entry.value.unwrap()).unwrap();
             }
         }
+        return Ok(());
     }
-    
+
     pub fn lookup(&self, key: &K) -> Option<V> {
         let index1 = self.hash1(key);
         let index2 = self.hash2(key);
