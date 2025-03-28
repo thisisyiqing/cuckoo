ec92869176a54ff1327bff4f2292c5eaf3eb6b48
Marcin Anforowicz <manforowicz@gmail.com>
2025-03-27 21:16:47 -0700

    Added basic loom test

--- tests/loom_test.rs
+++ tests/loom_test.rs
@@ -6,43 +6,45 @@ use rand::{Rng, SeedableRng};
 
 #[test]
 fn test1() {
-    loom::model(test1_body);
+    // Test 10 times with different random entries
+    for seed in 0..10 {
+        loom::model(move || test1_body(seed));
+    }
 }
 
-fn test1_body() {
-    const THREADS: u32 = 2;
-
-    let ht = Arc::new(CuckooHashTable::new());
-
-    let mut handles = Vec::new();
-
-    fn thread_func(ht: Arc<CuckooHashTable<u32, u32>>, data: Vec<KeyVal<u32, u32>>) {
-        for entry in data {
-            ht.insert(entry.key, entry.value);
-        }
-    }
-
-    let mut rng = rand::rngs::StdRng::seed_from_u64(10);
-    let mut rand_nums: Vec<KeyVal<u32, u32>> = (0..3)
+fn test1_body(seed: u64) {
+    let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
+    let entries: Vec<KeyVal<u32, u32>> = (0..2)
         .map(|_| KeyVal {
             key: rng.random(),
             value: rng.random(),
         })
         .collect();
 
-    for _ in 0..THREADS {
-        let ht_clone = ht.clone();
-        let rand_nums = rand_nums.clone();
-        let handle = thread::spawn(move || thread_func(ht_clone, rand_nums));
-        handles.push(handle);
+    let ht = Arc::new(CuckooHashTable::new());
+
+    fn thread_func(ht: Arc<CuckooHashTable<u32, u32>>, entries: Vec<KeyVal<u32, u32>>) {
+        for entry in entries {
+            ht.insert(entry.key, entry.value);
+        }
     }
 
+    let mut handles = Vec::new();
+    for _ in 0..2 {
+        let ht_clone = ht.clone();
+        let entries = entries.to_vec();
+        let handle = thread::spawn(move || thread_func(ht_clone, entries));
+        handles.push(handle);
+    }
     for handle in handles {
         handle.join().unwrap();
     }
 
     let mut elems = ht.get_vec();
     elems.sort_by_key(|kv| kv.key);
-    rand_nums.sort_by_key(|kv| kv.key);
-    assert_eq!(elems, rand_nums);
+
+    let mut entries = entries.to_vec();
+
+    entries.sort_by_key(|kv| kv.key);
+    assert_eq!(elems, entries);
 }
